37f4fe25510eedb783e613c5de927fa0dcaa5ac5
Nándor Dudás <nandor.dudas@gmail.com>
2025-03-03 23:59:35 +0100

    refactor: try-catch utils
    Signed-off-by: Nándor Dudás <nandor.dudas@gmail.com>

--- website/app/utils/try-catch.ts
+++ website/app/utils/try-catch.ts
@@ -1,226 +1,95 @@
-export type Constructor<T> = new (...args: any[]) => T
+import type { Constructor, TryCatch } from '~~/types'
 
-export function raiseError(
-  message: string,
-  ErrorConstructor: Constructor<Error> = Error,
-  cause?: unknown,
-): never {
-  if (typeof message !== 'string')
-    throw new TypeError('Message must be a string.')
+export class Predicate {
+  static isFunction(value: unknown): value is CallableFunction {
+    return typeof value === 'function'
+  }
 
-  if (!(ErrorConstructor.prototype instanceof Error))
-    throw new TypeError('ErrorConstructor must be a subclass of Error.')
-
-  throw new ErrorConstructor(message, { cause })
-}
-
-export function identity<T>(value: T): T {
-  return value
-}
-
-export function isFunction(value: unknown): value is CallableFunction {
-  return typeof value === 'function'
-}
-
-export function ensureFunction(value: unknown): asserts value is CallableFunction {
-  if (!isFunction(value))
-    raiseError('Expected a function but received a non-function value.')
-}
-
-/*  */
-
-export interface Success<T> {
-  readonly ok: true
-  readonly value: T
-  readonly error?: never
-}
-
-export interface Failure<E extends Error = Error> {
-  readonly ok: false
-  readonly value?: never
-  readonly error: E
-}
-
-export type Result<T, E extends Error = Error> =
-  | Success<T>
-  | Failure<E>
-
-export const isSuccess = <T, E extends Error>(result: Result<T, E>): result is Success<T> => result.ok === true
-export const isFailure = <T, E extends Error>(result: Result<T, E>): result is Failure<E> => result.ok === false
-export const createSuccess = <T>(value: T): Success<T> => ({ ok: true, value } as const)
-export const createFailure = <E extends Error>(error: E): Failure<E> => ({ ok: false, error } as const)
-
-export function assertSuccess<T, E extends Error>(result: Result<T, E>): asserts result is Success<T> {
-  if (!isSuccess(result)) {
-    raiseError(
-      `Expected a success result, but got a failure: ${result.error.message}`,
-      result.error.constructor as Constructor<Error>,
-      result.error.cause,
-    )
+  static instanceOf<T>(value: unknown, constructor: Constructor<T>): value is T {
+    return value instanceof constructor
   }
 }
 
-export function unwrapOr<T, E extends Error>(
-  result: Result<T, E>,
-  defaultValue: T,
-): T {
-  return result.ok
-    ? result.value
-    : defaultValue
-}
-
-export function unwrapOrThrow<T, E extends Error>(result: Result<T, E>): T {
-  assertSuccess(result)
-
-  return result.value
-}
-
-export function map<T, U, E extends Error>(
-  result: Result<T, E>,
-  fn: (value: T) => U,
-): Result<U, E> {
-  return result.ok
-    ? createSuccess(fn(result.value))
-    : result
-}
-
-export function tap<T, E extends Error>(
-  result: Result<T, E>,
-  fn: (value: T) => void,
-): Result<T, E> {
-  if (result.ok)
-    fn(result.value)
-
-  return result
-}
-
-export function combine<A, B, E extends Error>(
-  resultA: Result<A, E>,
-  resultB: Result<B, E>,
-): Result<[A, B], E> {
-  return resultA.ok
-    ? resultB.ok
-      ? createSuccess([resultA.value, resultB.value])
-      : resultB
-    : resultA
-}
-
-export function recover<T, U, E extends Error>(
-  result: Result<T, E>,
-  fn: (error: E) => U,
-): Result<T | U, never> {
-  return result.ok
-    ? result
-    : createSuccess(fn(result.error))
-}
-
-export function chain<T, U, E extends Error>(
-  result: Result<T, E>,
-  fn: (value: T) => Result<U, E>,
-): Result<U, E> {
-  return result.ok
-    ? fn(result.value)
-    : result
-}
-
-export function mapError<T, E extends Error, F extends Error>(
-  result: Result<T, E>,
-  fn: (error: E) => F,
-): Result<T, F> {
-  return result.ok
-    ? result
-    : createFailure(fn(result.error))
-}
-
-export interface MatchHandlers<T, E extends Error, R> {
-  onSuccess: (value: T) => R
-  onFailure: (error: E) => R
-}
-
-export function match<T, E extends Error, R>(
-  result: Result<T, E>,
-  handlers: MatchHandlers<T, E, R>,
-): R
-export function match<T, E extends Error, R>(
-  result: Promise<Result<T, E>>,
-  handlers: MatchHandlers<T, E, R>,
-): Promise<R>
-
-export function match<T, E extends Error, R>(
-  result: Result<T, E> | Promise<Result<T, E>>,
-  handlers: MatchHandlers<T, E, R>,
-): R | Promise<R> {
-  if (result instanceof Promise) {
-    return result
-      .then(result => match(result, handlers))
-      .catch(error => handlers.onFailure(defaultTransformError(error) as E))
+export class Assert {
+  static ensureFunction(fn: unknown): asserts fn is CallableFunction {
+    if (!Predicate.isFunction(fn))
+      throw new TypeError('fn must be a function.')
   }
-
-  return result.ok
-    ? handlers.onSuccess(result.value)
-    : handlers.onFailure(result.error)
 }
 
-type SyncFunction<T = any> = (...args: any[]) => T
-type AsyncFunction<T = any> = (...args: any[]) => Promise<T>
+export class Try {
+  static sync<T, E extends Error>(fn: () => T): TryCatch.Result<T, E> {
+    try {
+      Assert.ensureFunction(fn)
 
-export type AnyFunction<T = any> =
-  | SyncFunction<T>
-  | AsyncFunction<T>
+      const result = fn()
 
-export type TryCatchReturn<F extends AnyFunction, E extends Error = Error> =
-  ReturnType<F> extends Promise<infer R>
-    ? Promise<Result<R, E>>
-    : Result<ReturnType<F>, E>
-
-export interface ResultErrorOptions<E extends Error = Error> {
-  transformError?: (error: unknown) => E
-}
-
-function defaultTransformError<E extends Error = Error>(error: unknown): E {
-  return (
-    error instanceof Error
-      ? error
-      : new Error(String(error))
-  ) as E
-}
-
-export function tryCatch<T, E extends Error>(fn: SyncFunction<T>): TryCatchReturn<SyncFunction<T>, E>
-export function tryCatch<T, E extends Error>(fn: AsyncFunction<T>): TryCatchReturn<AsyncFunction<T>, E>
-export function tryCatch<T, E extends Error>(
-  fn: SyncFunction<T>,
-  options: ResultErrorOptions,
-): TryCatchReturn<SyncFunction<T>, E>
-export function tryCatch<T, E extends Error>(
-  fn: AsyncFunction<T>,
-  options: ResultErrorOptions,
-): TryCatchReturn<AsyncFunction<T>, E>
-
-export function tryCatch<T, E extends Error>(
-  fn: AnyFunction<T>,
-  options: ResultErrorOptions = { transformError: defaultTransformError },
-): TryCatchReturn<AnyFunction<T>, E> {
-  ensureFunction(fn)
-
-  function handleFailure(error: unknown): Failure<Error> {
-    if (options.transformError)
-      return createFailure(options.transformError(error))
-
-    return createFailure(defaultTransformError(error))
-  }
-
-  try {
-    const result = fn()
-
-    if (result instanceof Promise) {
-      return result
-        .then(createSuccess)
-        .catch(handleFailure) as TryCatchReturn<AnyFunction<T>, E>
+      return Result.createSuccess(result)
     }
-
-    return createSuccess(result) as TryCatchReturn<AnyFunction<T>, E>
+    catch (error) {
+      return Result.createFailureFrom<E>(error)
+    }
   }
-  catch (error) {
-    return handleFailure(error) as TryCatchReturn<AnyFunction<T>, E>
+
+  static async async<T, E extends Error>(fn: Promise<T>): Promise<TryCatch.Result<T, E>>
+  static async async<T, E extends Error>(fn: () => Promise<T>): Promise<TryCatch.Result<T, E>>
+  static async async<T, E extends Error>(fn: (() => Promise<T>) | Promise<T>): Promise<TryCatch.Result<T, E>> {
+    try {
+      if (Predicate.instanceOf(fn, Promise)) {
+        const result = await fn
+
+        return Result.createSuccess(result)
+      }
+
+      Assert.ensureFunction(fn)
+
+      const result = await fn()
+
+      return Result.createSuccess(result)
+    }
+    catch (error) {
+      return Result.createFailureFrom<E>(error)
+    }
+  }
+}
+
+class Result {
+  static destructure<T, E extends Error>(result: TryCatch.Result<T, E>) {
+    if (result.ok)
+      return { data: result.value, error: null }
+
+    return { data: null, error: result.error }
+  }
+
+  static isSuccess<T, E extends Error>(result: TryCatch.Result<T, E>): result is TryCatch.Success<T> {
+    return result.ok
+  }
+
+  static isFailure<T, E extends Error>(result: TryCatch.Result<T, E>): result is TryCatch.Failure<E> {
+    return !result.ok
+  }
+
+  static createSuccess<T>(value: T): TryCatch.Success<T> {
+    return { ok: true, value }
+  }
+
+  static createFailure<E extends Error>(error: E): TryCatch.Failure<E> {
+    return { ok: false, error }
+  }
+
+  static createFailureFrom<E extends Error>(value: unknown): TryCatch.Failure<E> {
+    const error = (value instanceof Error ? value : new Error(String(value))) as E
+
+    return Result.createFailure(error)
+  }
+
+  static match<T, E extends Error>(
+    result: TryCatch.Result<T, E>,
+    options: { onResult: (result: T) => T, onFailure: (error: E) => E } = {
+      onResult: value => value,
+      onFailure: error => error,
+    },
+  ): T | E {
+    return result.ok ? options.onResult(result.value) : options.onFailure(result.error)
   }
 }
--- website/types/index.d.ts
+++ website/types/index.d.ts
@@ -8,4 +8,22 @@ declare module 'nuxt/schema' {
   }
 }
 
+export type Constructor<T> = new (...args: any[]) => T
+
+export declare namespace TryCatch {
+  interface Success<T> {
+    readonly ok: true
+    readonly value: T
+    readonly error?: never
+  }
+
+  interface Failure<E extends Error> {
+    readonly ok: false
+    readonly value?: never
+    readonly error: E
+  }
+
+  type Result<T, E extends Error> = Success<T> | Failure<E>
+}
+
 export { }
